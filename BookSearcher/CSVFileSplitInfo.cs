3429faa7257a3fef97f95eeedd765340f99e7b7d
Shingo MORI <babydaemons@live.jp>
2022-05-15 19:27:54 +0900

    日本の古書屋のCSVファイル読み取り処理のリファクタリング

---
+++ BookSearcher/CSVFileSplitInfo.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic.FileIO;
+
+namespace BookSearcher
+{
+    internal abstract class CSVFileSplitInfo : CSVFile
+    {
+        const int CHECK_LINES = 10;
+        protected abstract string Url { get; }
+        protected abstract Regex RegexDelimiter { get; }
+        protected int infoIndex = -1;
+        protected int infoCount = -1;
+
+        public CSVFileSplitInfo(string path) : base(path)
+        {
+        }
+
+        public override bool ParseTitle()
+        {
+            if (!IsMatchUrl())
+            {
+                return false;
+            }
+
+            using (var reader = new TextFieldParser(Path, FileEncoding))
+            {
+                reader.SetDelimiters(",");
+                var titles = new List<string>(reader.ReadFields());
+                var fields = new List<string>(reader.ReadFields());
+                DetectInfoColumn(fields);
+                InsertTitleColums(titles);
+                InsertInfoColumn(fields);
+                Titles = titles.ToArray();
+                Fields = fields.ToArray();
+                if (Fields.Length == Titles.Length)
+                {
+                    Columns = Titles.Length;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        abstract protected void InsertTitleColums(List<string> titles);
+
+        private bool IsMatchUrl()
+        {
+            using (var reader = new StreamReader(Path, FileEncoding))
+            {
+                var line = reader.ReadLine();
+                for (int i = 0; i < CHECK_LINES; i++)
+                {
+                    line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    if (line.Contains(Url))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void DetectInfoColumn(List<string> fields)
+        {
+            for (int i = 0; i < fields.Count; i++)
+            {
+                var infos = RegexDelimiter.Split(fields[i]);
+                if (infos.Length > infoCount)
+                {
+                    infoIndex = i;
+                    infoCount = infos.Length;
+                }
+            }
+        }
+
+        private void InsertInfoColumn(List<string> fields)
+        {
+            var infos = RegexDelimiter.Split(fields[infoIndex]);
+            InsertInfoColumn(fields, infos);
+        }
+
+        protected abstract void InsertInfoColumn(List<string> fields, string[] infos);
+
+        protected override void DoReadAll()
+        {
+            using (var reader = new TextFieldParser(Path, FileEncoding))
+            {
+                reader.SetDelimiters(",");
+                _ = reader.ReadFields();
+                while (!reader.EndOfData)
+                {
+                    var fields = new List<string>(reader.ReadFields());
+                    InsertInfoColumn(fields);
+                    Records.Add(fields.ToArray());
+                }
+            }
+        }
+    }
+}
--- BookSearcher/CSVKoshoFile.cs
+++ BookSearcher/CSVKoshoFile.cs
@@ -2,26 +2,17 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text.RegularExpressions;
-using Microsoft.VisualBasic.FileIO;
-#if DEBUG
-using System.IO;
-#endif
 
 namespace BookSearcher
 {
-    internal class CSVKoshoFile : CSVFile
+    internal class CSVKoshoFile : CSVFileSplitInfo
     {
-        private CultureInfo culture;
-        private static readonly string url = "https://www.kosho.or.jp/products/";
-        private static readonly Regex regex_delimiter = new Regex("\\s+、");
-        private static readonly Regex regex_year = new Regex(@"^(?<year>([1１][9９]|[2２][0０])[0-9０-９][0-9０-９])");
-        private static readonly Regex regex_jyear = new Regex(@"^(?<year>(明治|大正|昭和|平成|令和)[1-9１-９][0-9０-９]?)");
-        private static readonly Regex regex_jyear2 = new Regex(@"^(?<era>[明大昭平令])(?<year>[1-9１-９][0-9０-９]?)");
-        private int infoIndex = -1;
-        private int infoCount = -1;
-#if DEBUG
-        private StreamWriter writer;
-#endif
+        private readonly CultureInfo culture;
+        protected override string Url => "https://www.kosho.or.jp/products/";
+        protected override Regex RegexDelimiter => new Regex("\\s+、");
+        private static readonly Regex RegexYear = new Regex(@"^(?<year>([1１][9９]|[2２][0０])[0-9０-９][0-9０-９])");
+        private static readonly Regex RegexJYear = new Regex(@"^(?<year>(明治|大正|昭和|平成|令和)[1-9１-９][0-9０-９]?)");
+        private static readonly Regex RegexJYear2 = new Regex(@"^(?<era>[明大昭平令])(?<year>[1-9１-９][0-9０-９]?)");
 
         public CSVKoshoFile(string path) : base(path)
         {
@@ -29,61 +20,16 @@ namespace BookSearcher
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
         }
 
-        public override bool ParseTitle()
+        protected override void InsertTitleColums(List<string> titles)
         {
-            bool result = false;
-            using (var reader = new TextFieldParser(Path, FileEncoding))
-            {
-                reader.SetDelimiters(",");
-                var titles = new List<string>(reader.ReadFields());
-                var fields = new List<string>(reader.ReadFields());
-                DetectInfoColumn(fields);
-                var titleBase = titles[infoIndex];
-                titles[infoIndex] += "/出版年";
-                titles.Insert(infoIndex, $"{titleBase}/出版社");
-                titles.Insert(infoIndex, $"{titleBase}/著者");
-                InsertInfoColumn(fields);
-                Titles = titles.ToArray();
-                Fields = fields.ToArray();
-                if (Fields.Length == Titles.Length)
-                {
-                    foreach (var field in Fields)
-                    {
-                        if (field.StartsWith(url))
-                        {
-                            RecordType = RecordType.KoshoLine;
-                            Columns = Titles.Length;
-                            result = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            return result;
+            var titleBase = titles[infoIndex];
+            titles[infoIndex] += "/出版年";
+            titles.Insert(infoIndex, $"{titleBase}/出版社");
+            titles.Insert(infoIndex, $"{titleBase}/著者");
         }
 
-        private void DetectInfoColumn(List<string> fields)
+        protected override void InsertInfoColumn(List<string> fields, string[] infos)
         {
-            for (int i = 0; i < fields.Count; i++)
-            {
-                var infos = regex_delimiter.Split(fields[i]);
-                if (infos.Length > infoCount)
-                {
-                    infoIndex = i;
-                    infoCount = infos.Length;
-                }
-            }
-        }
-
-        private void InsertInfoColumn(List<string> fields)
-        {
-            var infos = regex_delimiter.Split(fields[infoIndex]);
-#if DEBUG
-            if (writer != null)
-            {
-                writer.WriteLine(string.Join("\t", infos)); 
-            }
-#endif
             fields[infoIndex] = ParseYear(infos);
             fields.Insert(infoIndex, infos.Length > 1 ? infos[1] : "");
             fields.Insert(infoIndex, infos[0]);
@@ -93,26 +39,25 @@ namespace BookSearcher
         {
             foreach (var info in infos)
             {
-                var match_year = regex_year.Match(info);
+                var match_year = RegexYear.Match(info);
                 if (match_year.Success)
                 {
                     var year = match_year.Groups["year"].Value;
                     ConertWideDigits(ref year);
                     return year;
                 }
-                var match_jyear = regex_jyear.Match(info);
+                var match_jyear = RegexJYear.Match(info);
                 if (match_jyear.Success)
                 {
                     var year = match_jyear.Groups["year"].Value + "年";
                     ConertWideDigits(ref year);
-                    DateTime date;
-                    if (DateTime.TryParseExact(year, "ggyy年", culture, DateTimeStyles.AssumeLocal, out date))
+                    if (DateTime.TryParseExact(year, "ggyy年", culture, DateTimeStyles.AssumeLocal, out DateTime date))
                     {
                         year = date.Year.ToString();
                     }
                     return year;
                 }
-                var match_jyear2 = regex_jyear.Match(info);
+                var match_jyear2 = RegexJYear2.Match(info);
                 if (match_jyear2.Success)
                 {
                     var era = match_jyear2.Groups["era"].Value;
@@ -123,8 +68,7 @@ namespace BookSearcher
                     era = era.Replace("令", "令和");
                     var year = era + match_jyear2.Groups["year"].Value + "年";
                     ConertWideDigits(ref year);
-                    DateTime date;
-                    if (DateTime.TryParseExact(year, "ggyy年", culture, DateTimeStyles.AssumeLocal, out date))
+                    if (DateTime.TryParseExact(year, "ggyy年", culture, DateTimeStyles.AssumeLocal, out DateTime date))
                     {
                         year = date.Year.ToString();
                     }
@@ -146,24 +90,6 @@ namespace BookSearcher
             digits = digits.Replace("７", "7");
             digits = digits.Replace("８", "8");
             digits = digits.Replace("９", "9");
-        }
-
-        protected override void DoReadAll()
-        {
-#if DEBUG
-            using (writer = new StreamWriter(Path.Replace(".csv", ".tsv"), false, FileEncoding))
-#endif
-            using (var reader = new TextFieldParser(Path, FileEncoding))
-            {
-                reader.SetDelimiters(",");
-                _ = reader.ReadFields();
-                while (!reader.EndOfData)
-                {
-                    var fields = new List<string>(reader.ReadFields());
-                    InsertInfoColumn(fields);
-                    Records.Add(fields.ToArray());
-                }
-            }
         }
     }
 }
