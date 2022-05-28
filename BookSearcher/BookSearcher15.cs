497e176ab2d97dcc86993277d8632821fd47d5d6
MORI Shingo <babydaemons@gmail.com>
2022-05-28 09:44:02 +0900

    照合パターン⑮のユニットテスト

--- BookSearcher/BookSearcher15.cs
+++ BookSearcher/BookSearcher15.cs
@@ -2,7 +2,7 @@
 
 namespace BookSearcherApp
 {
-    internal class BookSearcher15 : BookSearcher
+    public class BookSearcher15 : BookSearcher
     {
         private readonly ColumnInfo bookTitle = new ColumnInfo(MatchType.CompleteMatch, SpaceMatch, ColumnType.BookTitle);
 
---
+++ BookSearcherTest/TestBookSearcher15.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookSearcherApp;
+
+namespace BookSearcherTest
+{
+    [TestClass]
+    public class TestBookSearcher15 : TestBookSearcher
+    {
+        private readonly CSVData dataAsc;
+        private readonly CSVData dataDesc;
+        private readonly CSVData dataDiff1;
+        private readonly CSVData dataDiff2;
+
+        public TestBookSearcher15()
+        {
+            const string BookTitle = "書籍タイトル";
+            const string valuePrefix = "書籍タイトル通巻";
+            BookColumnSetting.Rows.Add(BookTitle, valuePrefix, "書籍名");
+            ScrapingColumnSetting.Rows.Add(BookTitle, valuePrefix, "書籍名");
+
+            dataAsc = CreateDataAsc(BookTitle, valuePrefix, ROW_COUNT);
+            dataDesc = CreateDataDesc(BookTitle, valuePrefix, ROW_COUNT);
+            dataDiff1 = CreateDataAsc(BookTitle, valuePrefix, ROW_COUNT + 1);
+            dataDiff2 = CreateDataDesc(BookTitle, valuePrefix, ROW_COUNT + 1);
+        }
+
+        [TestMethod]
+        public void TestMatchingAscVsAsc()
+        {
+            var books = dataAsc;
+            var scrapings = dataAsc;
+            BookSearcher.InitSearchSettings(books, scrapings, SpaceMatch.All, -1);
+            searcher = new BookSearcher15();
+            searcher.Search();
+            Assert.AreEqual(ROW_COUNT, BookSearcher.ResultTable.Rows.Count);
+        }
+
+        [TestMethod]
+        public void TestMatchingAscVsDesc()
+        {
+            var books = dataAsc;
+            var scrapings = dataDesc;
+            BookSearcher.InitSearchSettings(books, scrapings, SpaceMatch.All, -1);
+            searcher = new BookSearcher15();
+            searcher.Search();
+            Assert.AreEqual(ROW_COUNT, BookSearcher.ResultTable.Rows.Count);
+        }
+
+        [TestMethod]
+        public void TestMatchingDiffRows1()
+        {
+            var books = dataAsc;
+            var scrapings = dataDiff1;
+            BookSearcher.InitSearchSettings(books, scrapings, SpaceMatch.All, -1);
+            searcher = new BookSearcher15();
+            searcher.Search();
+            Assert.AreEqual(ROW_COUNT, BookSearcher.ResultTable.Rows.Count);
+        }
+
+        [TestMethod]
+        public void TestMatchingDiffRows2()
+        {
+            var books = dataDiff2;
+            var scrapings = dataAsc;
+            BookSearcher.InitSearchSettings(books, scrapings, SpaceMatch.All, -1);
+            searcher = new BookSearcher15();
+            searcher.Search();
+            Assert.AreEqual(ROW_COUNT, BookSearcher.ResultTable.Rows.Count);
+        }
+    }
+}
