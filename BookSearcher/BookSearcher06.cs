cb37ae0a285564edabdc4a56c361f7d9a2750672
MORI Shingo <babydaemons@gmail.com>
2022-05-28 12:41:58 +0900

    照合パターン⑥のユニットテスト

--- BookSearcher/BookSearcher06.cs
+++ BookSearcher/BookSearcher06.cs
@@ -2,7 +2,7 @@
 
 namespace BookSearcherApp
 {
-    internal class BookSearcher06 : BookSearcher
+    public class BookSearcher06 : BookSearcher
     {
         private readonly ColumnInfo bookTitle = new ColumnInfo(MatchType.PartialMatch, SpaceMatch, ColumnType.BookTitle);
         private readonly ColumnInfo publisher = new ColumnInfo(MatchType.CompleteMatch, SpaceMatch.Ignore, ColumnType.Publisher);
