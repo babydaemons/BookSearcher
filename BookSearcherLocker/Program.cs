5e21e246577130942285898db5cad5a9c948e384
MORI Shingo <babydaemons@gmail.com>
2022-07-03 05:56:09 +0900

    ファイルを吐いた処理するテストプログラムを追加

---
+++ BookSearcherLocker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSearcherLocker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Lock Target File: ");
+            var path = Console.ReadLine();
+            using (var file = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                Console.Write("Enter To Exit: ");
+                _ = Console.ReadLine();
+            }
+        }
+    }
+}
